e1504033af6cf81ab0c13a36327f1f87517255a4
ferriyusra <ferisakuragi@gmail.com>
2023-05-10 15:16:45 +0700

    create register api

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable, HttpStatus } from '@nestjs/common';
+import { RegisterDto } from './dto/RegisterDto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private prisma: PrismaService) {}
+
+  /**
+   * Register new user
+   * @param data
+   * @returns
+   */
+  async register(data: RegisterDto) {
+    const checkUser = await this.prisma.users.findFirst({
+      where: {
+        email: data.email,
+      },
+    });
+
+    if (checkUser) {
+      throw new HttpException('User alredy registered', HttpStatus.FOUND);
+    }
+
+    const createUser = await this.prisma.users.create({
+      data: data,
+    });
+
+    if (createUser) {
+      return {
+        status_code: 200,
+        message: 'Register Successfully',
+      };
+    }
+  }
+}
